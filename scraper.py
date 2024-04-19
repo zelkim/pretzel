06c8102b6e424dbd893b089f949e4f2e46111bed
sampdevi <dev86.sean@gmail.com>
2024-04-19 20:51:57 +0800

    changed open from w -> w+

--- scraper.py
+++ scraper.py
@@ -19,7 +19,7 @@ async def main(course: str, id: str):
         elem = await driver.find_element(By.XPATH, '/html/body/table[4]/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr/td[2]/form/table', timeout=10)
         print("FOUND ELEM");
         content = str(await elem.text).replace('\xa0\xa0', '').split("\n")
-        with open('raw/'+course.lower()+'.json', 'w') as f:
+        with open('raw/'+course.lower()+'.json', 'w+') as f:
             f.write(json.dumps(content))
 
 for i in range(1, len(sys.argv)):
