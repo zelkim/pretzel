09f7df4b6cfa79ed106999cf1acf87ca6b9fcd4c
sampdevi <dev86.sean@gmail.com>
2024-04-19 20:23:56 +0800

    removed ping, added help cmd, changed gitignore

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,8 @@
 module.exports = {
     config: {
-        name: 'ping',
-        description: 'Get ping of the bot',
-        usage: `!ping`,
+        name: 'help',
+        description: 'A guide on how to use the bot.',
+        usage: `!help`,
     },
     async run(bot, message, args) {
         message.channel.send("My ping is \`" + bot.ws.ping + " ms\`");
