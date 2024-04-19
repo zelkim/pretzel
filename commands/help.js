46fe4f2d3e419ee8c851ad3869d60376e691c9d7
sampdevi <dev86.sean@gmail.com>
2024-04-19 20:30:13 +0800

    added help embed message

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,5 @@
+const { EmbedBuilder } = require('discord.js')
+
 module.exports = {
     config: {
         name: 'help',
@@ -5,6 +7,16 @@ module.exports = {
         usage: `!help`,
     },
     async run(bot, message, args) {
-        message.channel.send("My ping is \`" + bot.ws.ping + " ms\`");
+        const embed = new EmbedBuilder()
+            .setTitle("pretzel")
+            .setDescription("Access the DLSU Course Offerings through discord!\n\n**COMMANDS:**\n```\n!open <course code>\n!class <course code> <class ID or section>\n```\nExample usage:\n`!open CCPROG3`\n`!open LCFAITH z29`")
+            .setThumbnail("https://i.imgur.com/meFtxzo.png")
+            .setColor("#a3ffc3")
+            .setFooter({
+                text: "Developed by zel :)",
+                iconURL: "https://i.imgur.com/meFtxzo.png",
+            });
+
+        await message.channel.send({ embeds: [embed] });
     }
 }
